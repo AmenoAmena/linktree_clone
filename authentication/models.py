cc574a086542a69aa9ba11803f8bf4f8e41c4a92
AmenoAmena <ardacavusoglu666@gmail.com>
2024-07-04 13:06:53 +0300

    before user migration

---
+++ authentication/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+#class User(AbstractUser):
+#    link1 = models.URLField()
+#    link2 = models.URLField()
+#    link3 = models.URLField()
+#    link4 = models.URLField()
+
+
+
+
